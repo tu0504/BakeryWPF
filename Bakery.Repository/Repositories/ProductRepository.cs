013fb9182bd030f9e72fe77fc214ff46c28fc9df
tuanvy <maituanvyst@gmail.com>
2025-11-11 09:59:48 +0700

    CRUD API of Product

---
+++ Bakery.Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,48 @@
+﻿
+using Bakery.Repository.Context;
+using Bakery.Repository.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bakery.Repository.Repositories
+{
+    public class ProductRepository
+    {
+     public BakeryContext _context;
+
+        public List <Product> GetAll()
+        {
+            _context = new BakeryContext();
+            return _context.Products.Include("Category").ToList();
+        }
+
+        public void Create(Product product)
+        {
+            _context = new BakeryContext();
+             _context.Products.Add(product);
+            _context.SaveChanges();
+        }
+        public void Update(Product product)
+        {
+            _context = new BakeryContext();
+            _context.Products.Update(product);
+            _context.SaveChanges();
+        }
+        public void Delete(Product product)
+        {
+            _context = new BakeryContext();
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+        }
+        public Product GetById(int id)
+        {
+            _context = new();
+            return _context.Products.Include("Category").FirstOrDefault(p => p.ProductId == id);
+        }
+
+        public List<Product> GetByName(string name)
+        {
+            _context = new BakeryContext();
+            return _context.Products.Include(p => p.Category).Where(p => p.ProductName.Contains(name)).ToList();
+        }
+    }
+}
--- Bakery.WpfApplication/obj/Debug/net8.0-windows/App.g.i.cs
+++ Bakery.WpfApplication/obj/Debug/net8.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace Bakery.WpfApplication {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.9.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace Bakery.WpfApplication {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.9.0")]
         public static void Main() {
             Bakery.WpfApplication.App app = new Bakery.WpfApplication.App();
             app.InitializeComponent();
