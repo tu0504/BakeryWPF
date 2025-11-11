6951c07eb3cdbb2fe24eaa80e1859e05ee56abf6
tuanvy <maituanvyst@gmail.com>
2025-11-11 10:51:06 +0700

    CRUD Category service

---
+++ Bakery.Repository/Repositories/CategoryRepository.cs
@@ -0,0 +1,41 @@
+﻿
+
+using Bakery.Repository.Context;
+using Bakery.Repository.Models;
+
+namespace Bakery.Repository.Repositories
+{
+    public class CategoryRepository
+    {
+        public BakeryContext _context;
+
+        public CategoryRepository(BakeryContext context) { _context = context; }
+
+        public List<Category> GetAll()
+        {
+            return _context.Categories.ToList();
+        }
+
+        public Category GetById(int id)
+        {
+            return _context.Categories.FirstOrDefault(c => c.CategoryId == id);
+        }
+
+        public void Create(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+        }
+        public void Update (Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+        }
+        public void Delete (Category category)
+        {
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+        }
+
+    }
+}
